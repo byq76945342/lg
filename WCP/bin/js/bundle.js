559ea25d87af866a07138d3f087f8102267b2771
byq769465342 <2529851191@qq.com>
2021-01-06 16:40:07 +0800

    ti jiao xiu gai

---
+++ WCP/bin/js/bundle.js
@@ -0,0 +1,68 @@
+(function () {
+    'use strict';
+
+    class GameConfig {
+        constructor() { }
+        static init() {
+            var reg = Laya.ClassUtils.regClass;
+        }
+    }
+    GameConfig.width = 640;
+    GameConfig.height = 1136;
+    GameConfig.scaleMode = "fixedwidth";
+    GameConfig.screenMode = "none";
+    GameConfig.alignV = "top";
+    GameConfig.alignH = "left";
+    GameConfig.startScene = "LoadingView.scene";
+    GameConfig.sceneRoot = "";
+    GameConfig.debug = false;
+    GameConfig.stat = false;
+    GameConfig.physicsDebug = false;
+    GameConfig.exportSceneToJson = true;
+    GameConfig.init();
+
+    class Main {
+        constructor() {
+            let needChangeHW = Laya.Browser.clientHeight > Laya.Browser.clientWidth;
+            let borwerH = needChangeHW ? Laya.Browser.clientHeight : Laya.Browser.clientWidth;
+            let browerW = needChangeHW ? Laya.Browser.clientWidth : Laya.Browser.clientHeight;
+            let rhw = borwerH / browerW;
+            let dhw = GameConfig.height / GameConfig.width;
+            let isFullScreen = rhw > dhw;
+            if (isFullScreen) {
+                GameConfig.height = borwerH / browerW * GameConfig.width;
+                GameConfig.height = GameConfig.height | 0;
+            }
+            if (window["Laya3D"])
+                Laya3D.init(GameConfig.width, GameConfig.height);
+            else
+                Laya.init(GameConfig.width, GameConfig.height, Laya["WebGL"]);
+            Laya["Physics"] && Laya["Physics"].enable();
+            Laya["DebugPanel"] && Laya["DebugPanel"].enable();
+            Laya.stage.scaleMode = isFullScreen ? Laya.Stage.SCALE_FIXED_WIDTH : Laya.Stage.SCALE_FIXED_AUTO;
+            Laya.stage.screenMode = GameConfig.screenMode;
+            Laya.stage.screenAdaptationEnabled = true;
+            Laya.stage.alignV = GameConfig.alignV;
+            Laya.stage.alignH = GameConfig.alignH;
+            Laya.stage.bgColor = "#ff1542";
+            Laya.URL.exportSceneToJson = GameConfig.exportSceneToJson;
+            if (GameConfig.debug || Laya.Utils.getQueryString("debug") == "true")
+                Laya.enableDebugPanel();
+            if (GameConfig.physicsDebug && Laya["PhysicsDebugDraw"])
+                Laya["PhysicsDebugDraw"].enable();
+            if (GameConfig.stat)
+                Laya.Stat.show();
+            Laya.alertGlobalError(true);
+            Laya.ResourceVersion.enable("version.json", Laya.Handler.create(this, this.onVersionLoaded), Laya.ResourceVersion.FILENAME_VERSION);
+        }
+        onVersionLoaded() {
+            Laya.AtlasInfoManager.enable("fileconfig.json", Laya.Handler.create(this, this.onConfigLoaded));
+        }
+        onConfigLoaded() {
+            GameConfig.startScene && Laya.Scene.open(GameConfig.startScene);
+        }
+    }
+    new Main();
+
+}());
+//# sourceMappingURL=bundle.js.map
